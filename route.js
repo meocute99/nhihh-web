b8ee3f541e76ba9af25314f476c516039ab873ae
meocute99 <acctiktokus31@gmail.com>
2026-07-08 19:33:19 +0700

    Delete route.js

--- route.js
+++
@@ -1,9 +0,0 @@
-import { AdminApp } from "../../components/NhihhSystem";
-
-export const metadata = {
-  title: "NhiHH Link — Quản trị",
-};
-
-export default function AdminPage() {
-  return <AdminApp />;
-}
